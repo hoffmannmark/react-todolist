e7267ca37c450384476bc6288bca70038e69e18b
y.kostiuk <yaroslav.kostiuk@socialtechnologies.io>
2021-12-23 00:01:43 +0200

    Add new functional

--- App.js
+++ App.js
@@ -1,19 +1,87 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Platform, KeyboardAvoidingView, StyleSheet, Text, View, TextInput} from 'react-native';
 import Task from './components/task';
 import {TouchableOpacity} from "react-native-web";
 
+
+import Storage from 'react-native-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 export default function App() {
+
+    const storage = new Storage({
+        size: 1000,
+        storageBackend: AsyncStorage,
+        defaultExpires: null,
+        enableCache: true,
+    });
+
+
     const [task, setTask] = useState([]);
     const [taskItems, setTaskItems] = useState([]);
+
+    const InitialAllTasks = () => {
+        storage.getAllDataForKey('tasks').then(storageTasks => {
+            console.log(storageTasks);
+            if (storageTasks.length > 0) {
+                setTaskItems(storageTasks)
+            }
+        });
+    }
+
+    useEffect(() => {
+        InitialAllTasks();
+    }, [])
+
     const handleAddTask = () => {
-        setTaskItems([...taskItems, task])
+        setTaskItems([{text: task, status: 'new'}, ...taskItems])
+        storage.save({
+            key: 'tasks',
+            id: taskItems.length,
+            data: {text: task, status: 'new'},
+        });
         setTask('');
     };
-    const completeTask = (index) => {
+    const deleteTask = (index) => {
+        console.log('DELETE')
         let itemsCopy = [...taskItems];
         itemsCopy.splice(index, 1);
         setTaskItems(itemsCopy);
+        storage.clearMap();
+        for (let i = 0; itemsCopy.length > i; i++) {
+            storage.save({
+                key: 'tasks',
+                id: i,
+                data: itemsCopy[i],
+            });
+        }
+    }
+
+    const doneTask = (index) => {
+        console.log('doneTask')
+        let itemsCopy = [...taskItems];
+        let doneTaskItem = taskItems[index];
+        itemsCopy.splice(index, 1);
+        let allItemsCopyWithDone;
+        if (doneTaskItem.status === 'done') {
+            allItemsCopyWithDone = [doneTaskItem, ...itemsCopy]
+            doneTaskItem.status = 'new';
+        } else {
+            allItemsCopyWithDone = [...itemsCopy, doneTaskItem]
+            doneTaskItem.status = 'done';
+        }
+
+
+        setTaskItems(allItemsCopyWithDone);
+        storage.clearMap();
+        for (let i = 0; allItemsCopyWithDone.length > i; i++) {
+            storage.save({
+                key: 'tasks',
+                id: i,
+                data: allItemsCopyWithDone[i],
+            });
+        }
     }
 
     return (
@@ -23,9 +91,9 @@ export default function App() {
                 {
                     taskItems.map((item, index) => {
                         return (
-                            <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-                                <Task text={item}/>
-                            </TouchableOpacity>
+                            <View key={index}>
+                                <Task text={item.text} status={item.status} index={index} deletItemParent={deleteTask} doneItemParent={doneTask}/>
+                            </View>
                         )
                     })
                 }
@@ -41,6 +109,8 @@ export default function App() {
             </KeyboardAvoidingView>
         </View>
     );
+
+
 }
 
 const styles = StyleSheet.create({
@@ -51,17 +121,15 @@ const styles = StyleSheet.create({
     },
     sectionTitle: {
         fontSize: 32,
-        fontWeight: 800,
+        fontWeight: 'bold',
         paddingHorizontal: 16,
     },
     items: {
-        display: 'grid',
-        gridTemplateColumns: '1fr',
         paddingTop: 24,
         paddingHorizontal: 16,
+
     },
     writeTaskWrapper: {
-        boxSizing: 'border-box',
         position: 'absolute',
         bottom: 40,
         width: '100%',
@@ -89,15 +157,15 @@ const styles = StyleSheet.create({
     addText: {
         color: '#ffffff',
         fontSize: 40,
-        lineHeight: '100%',
         height: 48,
         width: 48,
-        borderRadius: '100%',
+        borderRadius: 100,
         backgroundColor: '#55BCF6',
         display: 'flex',
         justifyContent: 'center',
-
     }
 
 });
+
+
 
--- components/task.js
+++ components/task.js
@@ -3,15 +3,28 @@ import {View, Text, StyleSheet} from 'react-native';
 import {TouchableOpacity} from "react-native-web";
 
 const task = (props) => {
+    let callParentFunctionDelete = (dex) => {
+        props.deletItemParent(dex)
+    }
+    let callparentfunctionDone = (dex) => {
+        props.doneItemParent(dex)
+    }
+    console.log(props, 'TEXS');
     return (
         <View style={[styles.taskItem, styles.boxShadow]}>
-            <View style={styles.checkBox}>
-
+            <TouchableOpacity onPress={() => callparentfunctionDone(props.index )}>
+            <View style={[styles.checkBox, (props.status === "done") ? styles.checked : '']}>
             </View>
+            </TouchableOpacity>
             <View style={styles.left}>
                 <TouchableOpacity style={styles.checkbox}></TouchableOpacity>
-                <Text>{props.text}</Text>
+                <Text style={[styles.textDefault, (props.status === "done") ? styles.textDone : '']}>{props.text}</Text>
             </View>
+            <TouchableOpacity onPress={() =>  callParentFunctionDelete(props.index )}>
+                <View style={styles.deleteBtn} >
+
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -23,6 +36,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between',
         paddingHorizontal: 16,
         borderRadius: 16,
         borderWidth: 1,
@@ -36,15 +50,29 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         shadowRadius: 3,
     },
-    left: {
-    },
+    left: {},
     checkBox: {
         width: 24,
         height: 24,
         borderColor: '#55BCF6',
         borderWidth: 2,
-        borderRadius: '100%',
+        borderRadius: 100,
         marginRight: 16
+    },
+    checked: {
+        backgroundColor: '#1f9ade',
+    },
+    textDefault: {
+        color: 'black'
+    },
+    textDone: {
+        color: 'grey',
+    },
+    deleteBtn: {
+        width: 24,
+        height: 24,
+        backgroundColor: 'red',
+        borderRadius: 2
     }
 
 })
